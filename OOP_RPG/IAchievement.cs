4cfe5ccda844b39a38d92377998af0fe2a499c96
youngsoo shin <sys920@gmail.com>
2019-02-28 23:12:05 -0600

    update achievement

---
+++ OOP_RPG/IAchievement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_RPG
+{
+    public interface IAchievement
+    {
+        string Name { get; }
+        int Pont { get; }
+
+        bool Check(Hero hero);
+    }
+}
